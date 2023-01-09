6ce040376bf576fd99d4df2282eb5b1508fbf5fb
Patrick Diehl <me@diehlpk.de>
2023-01-09 13:22:43 +0100

    Clean script

--- benchmark.sh
+++ benchmark.sh
@@ -4,20 +4,20 @@ len=100000000
 path=$(pwd)
 # Taylor HPX parallel algorithms
 
-#for i in {64..1}
-#do
-#    ${path}/build/src/taylor_future ${len} 0.5 $i >> $1_taylor_future.csv
-#done
+for i in {64..1}
+do
+    ${path}/build/src/taylor_future ${len} 0.5 $i >> $1_taylor_future.csv
+done
 
-#for i in {64..1}
-#do
-#	srun  ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i >> $1_taylor_future_hpx.csv
-#done
+for i in {64..1}
+do
+	srun  ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i >> $1_taylor_future_hpx.csv
+done
 
-#for i in {64..1}
-#do
-#    srun  ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i >> $1_taylor_par_hpx.csv
-#done
+for i in {64..1}
+do
+    srun  ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i >> $1_taylor_par_hpx.csv
+done
 
 for i in {42..1}
 do
