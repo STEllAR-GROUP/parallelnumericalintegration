649681571bfde37be49725f439c14acb40b9ca28
Patrick Diehl <me@diehlpk.de>
2023-01-09 09:25:04 +0100

    Fix gcc 10

--- src/taylor_future_coroutine_hpx.cpp
+++ src/taylor_future_coroutine_hpx.cpp
@@ -44,7 +44,7 @@ hpx::future<double> run(size_t n, size_t amount, double x) {
 
   double result = 0;
 
-  auto futures2 = co_await hpx::when_all(std::move(futures));
+  auto futures2 = co_await std::move(hpx::when_all(std::move(futures)));
 
   for (size_t i = 0; i < futures2.size(); i++) result += co_await std::move(futures2[i]);
 
