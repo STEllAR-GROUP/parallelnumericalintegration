f821afacc45ec70ba5d8c06764ecfc27e55fa512
Chris Taylor <ct.clmsn@gmail.com>
2023-06-20 02:39:50 +0000

    updated for rv64g

--- benchmark.sh
+++ benchmark.sh
@@ -2,13 +2,14 @@
 
 len=100000000
 path=$(pwd)
+#25
 
-for i in {25..1}
+for i in {4..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun ${path}/build/src/taylor_future ${len} 0.5 $i)  
+    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future ${len} 0.5 $i)  
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -17,12 +18,12 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >> $1_taylor_future_median.csv
 done
 
-for i in {25..1}
+for i in {4..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun  ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i)
+    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i)
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -31,12 +32,12 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >> $1_taylor_future_hpx_median.csv
 done
 
-for i in {25..1}
+for i in {4..1}
 do
     data=""
     for j in {1..10}
     do	    
-    result=$(srun  ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i)
+    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i)
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -45,12 +46,12 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >>  $1_taylor_par_hpx_median.csv
 done
 
-for i in {1..1}
+for i in {4..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun  ${path}/build/src/taylor_future_coroutine_hpx ${len} 0.5 $i --hpx:threads=$i)
+    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_coroutine_hpx ${len} 0.5 $i --hpx:threads=$i)
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -60,12 +61,12 @@ do
 done
 
 
-for i in {64..1}
+for i in {4..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun  ${path}/build/src/taylor_sender_receiver_hpx ${len} 0.5 $i --hpx:threads=$i)
+    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_sender_receiver_hpx ${len} 0.5 $i --hpx:threads=$i)
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
