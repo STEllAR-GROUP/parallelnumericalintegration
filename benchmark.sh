6414d93066cd1dbb9ef79d8c2e4823e622fa2626
ctaylor <ctaylor@tactcomplabs.com>
2023-06-24 12:53:01 -0400

    added pine64 star64 support

--- benchmark.sh
+++ benchmark.sh
@@ -2,14 +2,25 @@
 
 len=100000000
 path=$(pwd)
-#25
 
-for i in {4..1}
+iter = 25
+
+if [ $1 = "rv64g" ]; then
+itr = 4
+elif [ $1 = "pine64" ]; then
+itr = 4
+fi
+
+for i in {itr..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future ${len} 0.5 $i)  
+    if [ $1 = "pine64" ]; then
+       result=$(${path}/build/src/taylor_future ${len} 0.5 $i)
+    else
+       result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future ${len} 0.5 $i)  
+    fi
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -18,12 +29,16 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >> $1_taylor_future_median.csv
 done
 
-for i in {4..1}
+for i in {itr..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i)
+    if [ $1 = "pine64" ]; then
+       result=$(${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i)
+    else
+       result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_hpx ${len} 0.5 $i --hpx:threads=$i)
+    fi
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -32,12 +47,16 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >> $1_taylor_future_hpx_median.csv
 done
 
-for i in {4..1}
+for i in {itr..1}
 do
     data=""
     for j in {1..10}
     do	    
-    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i)
+    if [ $1 = "pine64" ]; then
+       result=$(${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i)
+    else
+       result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_par_hpx ${len} 0.5 --hpx:threads=$i)
+    fi
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -46,12 +65,16 @@ do
     echo "$i,${list[${#list[*]}/2]},${list[0]},${list[${#list[*]}-1]}" >>  $1_taylor_par_hpx_median.csv
 done
 
-for i in {4..1}
+for i in {itr..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_coroutine_hpx ${len} 0.5 $i --hpx:threads=$i)
+    if [ $1 = "pine64" ]; then
+       result=$(${path}/build/src/taylor_future_coroutine_hpx ${len} 0.5 $i --hpx:threads=$i)
+    else
+       result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_future_coroutine_hpx ${len} 0.5 $i --hpx:threads=$i)
+    fi
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
@@ -61,12 +84,16 @@ do
 done
 
 
-for i in {4..1}
+for i in {itr..1}
 do
     data=""
     for j in {1..10}
     do
-    result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_sender_receiver_hpx ${len} 0.5 $i --hpx:threads=$i)
+    if [ $1 = "pine64" ]; then
+       result=$(${path}/build/src/taylor_sender_receiver_hpx ${len} 0.5 $i --hpx:threads=$i)
+    else
+       result=$(srun --cpus-per-task=4 --ntasks-per-node=1 --ntasks-per-core=1 ${path}/build/src/taylor_sender_receiver_hpx ${len} 0.5 $i --hpx:threads=$i)
+    fi
     array=($result)
     IFS=',' read -ra my_array <<< "${array[0]}"
     data="$data ${my_array[1]}"
