869e929e5a6a1ff8880cf71a705f7b4cc02ecf96
Patrick Diehl <me@diehlpk.de>
2024-02-20 21:28:50 -0600

    Fix code

--- src/set_future_hpx.cpp
+++ src/set_future_hpx.cpp
@@ -15,52 +15,49 @@
 
 typedef std::complex<double> complex;
 
+void launch(size_t i, size_t pixel_x, size_t pixel_y, PBM *pbm){
+
+    complex c = 
+        complex(0, 4) * complex(i, 0) / complex(pixel_x, 0) - complex(0, 2);
+
+    for(size_t j = 0 ; j < pixel_y ; j++){
+        int value = compute_pixel(c + 4.0 * j / pixel_y - 2.0);
+        //std::cout << value << std::endl;
+        std::tuple<size_t, size_t, size_t> color = get_rgb(value);
+        //std::cout << std::get<0>(color) << " "  <<std::get<1>(color) << " " << std::get<2>(color) << std::endl;
+        pbm->row(i)[j] = make_color(std::get<0>(color), std::get<1>(color),
+                                std::get<2>(color));
+        }
+}
+
+
+
+
 int main(int args, char** argv) {
   int pixel_x = std::stoi(argv[1]);
   int pixel_y = std::stoi(argv[2]);
+ 
+  std::cout << pixel_x << " " << pixel_y << std::endl;
+
+  PBM pbm(pixel_x,pixel_y);
   
   auto start = std::chrono::high_resolution_clock::now();
 
-  std::vector<hpx::future<std::vector<int>>> futures;
+  std::vector<hpx::future<void>> futures;
   
   for (size_t i = 0; i < pixel_x ; i++ )
   {
    
-
-    futures.push_back(std::move(hpx::async([&](){
-
-    std::vector<int> row;
-    complex c = 
-        complex(0, 4) * complex(i, 0) / complex(pixel_x, 0) - complex(0, 2);
-
-      for(size_t j = 0 ; j < pixel_y ; j++){
-
-        int value = compute_pixel(c + 4.0 * j / pixel_y - 2.0);
-        std::tuple<size_t, size_t, size_t> color = get_rgb(value);
-        row.push_back(make_color(std::get<0>(color), std::get<1>(color),
-                                std::get<2>(color)));
-
-    }
-    return row;
-
-    })));
+    futures.push_back(std::move(hpx::async(launch,i,pixel_x,pixel_y,&pbm)));
   }
 
-  hpx::when_all(futures).then([pixel_x,pixel_y](auto&& f){
-
-    auto futures = f.get();
-    PBM pbm(pixel_x,pixel_y);
-
-    for(size_t i = 0 ; i < pixel_x ; i++)
-        pbm.row(i) = futures[i].get();
-
-    
-    pbm.save("image_future.pbm");
-
-  }).get();
+  hpx::when_all(futures).get();
 
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = end - start;
   std::cout << pixel_x * pixel_y << "," << diff.count() << std::endl;
+  
+  pbm.save("image_future.pbm");
+  
   return EXIT_SUCCESS;
 }
--- src/util/kernel.hpp
+++ src/util/kernel.hpp
@@ -14,10 +14,12 @@ const double pi = M_PI;
 const int max_color = 256;
 
 size_t compute_pixel(std::complex<double> c) {
+    //std::cout <<  " compute " << c << std::endl; 
     std::complex<double> z(0, 0);
 
     for (size_t i = 0; i < max_iterations; i++) {   
         z = z * z + c;
+        //std::cout << "i " << i << std::endl;
         if (abs(z) > 2.0) {
                 return i;
         }
