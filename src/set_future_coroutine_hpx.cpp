f88c724cf7c9026ecece2ab7c28ae60e6ac57498
Patrick <patrickdiehl@lsu.edu>
2024-02-22 16:31:53 -0600

    Add set future coroutine

---
+++ src/set_future_coroutine_hpx.cpp
@@ -0,0 +1,63 @@
+//  Copyright (c) 2024 AUTHORS
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#include <cmath>
+#include <cstdlib>
+#include <hpx/hpx_main.hpp>
+#include <hpx/numeric.hpp>
+#include <hpx/parallel/algorithm.hpp>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#ifdef __GNUC__
+#ifndef __APPLE__
+#include <coroutine>
+#endif
+#endif
+
+hpx::future<void> run(size_t pixel_x, size_t pixel_y, PBM* pbm) {
+ 
+  std::vector<hpx::future<void>> futures;
+
+
+   for (size_t i = 0; i < pixel_x; i++) {
+    futures.push_back(std::move(hpx::async([pixel_x, pixel_y, &pbm](size_t i) {
+        complex c =
+            complex(0, 4) * complex(i, 0) / complex(pixel_x, 0) - complex(0, 2);
+
+        for (size_t j = 0; j < pixel_y; j++) {
+          int value = compute_pixel(c + 4.0 * j / pixel_y - 2.0);
+          std::tuple<size_t, size_t, size_t> color = get_rgb(value);
+          pbm.row(i)[j] = make_color(std::get<0>(color), std::get<1>(color),
+                                     std::get<2>(color));
+        }
+      })));
+  }
+
+  auto futures2 = co_await std::move(hpx::when_all(std::move(futures)));
+
+  co_return ;
+}
+
+int main(int args, char** argv) {
+  int pixel_x = std::stoi(argv[1]);
+  int pixel_y = std::stoi(argv[2]);
+
+  PBM pbm(pixel_x, pixel_y);
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  double result = run(n, amount, x).get();
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::cout << pixel_x * pixel_y << "," << diff.count() << std::endl;
+
+  pbm.save("image_future.pbm");
+
+  return EXIT_SUCCESS;
+}
