57404fc5de90e11314722909069fa97f34f432cf
Patrick <patrickdiehl@lsu.edu>
2024-02-23 11:30:41 -0600

    Fix code for the coroutines

--- src/set_future_coroutine_hpx.cpp
+++ src/set_future_coroutine_hpx.cpp
@@ -9,11 +9,11 @@
 #include <hpx/numeric.hpp>
 #include <hpx/parallel/algorithm.hpp>
 #include <iostream>
+#include <kernel.hpp>
 #include <numeric>
+#include <pbm.hpp>
 #include <string>
 #include <vector>
-#include <pbm.hpp>
-#include <kernel.hpp>
 
 #ifdef __GNUC__
 #ifndef __APPLE__
@@ -21,28 +21,28 @@
 #endif
 #endif
 
+typedef std::complex<double> complex;
+
 hpx::future<void> run(size_t pixel_x, size_t pixel_y, PBM* pbm) {
- 
   std::vector<hpx::future<void>> futures;
 
+  for (size_t i = 0; i < pixel_x; i++) {
+    futures.push_back(std::move(hpx::async([i, pixel_x, pixel_y, &pbm]() {
+      complex c =
+          complex(0, 4) * complex(i, 0) / complex(pixel_x, 0) - complex(0, 2);
 
-   for (size_t i = 0; i < pixel_x; i++) {
-    futures.push_back(std::move(hpx::async([pixel_x, pixel_y, &pbm](size_t i) {
-        complex c =
-            complex(0, 4) * complex(i, 0) / complex(pixel_x, 0) - complex(0, 2);
-
-        for (size_t j = 0; j < pixel_y; j++) {
-          int value = compute_pixel(c + 4.0 * j / pixel_y - 2.0);
-          std::tuple<size_t, size_t, size_t> color = get_rgb(value);
-          pbm.row(i)[j] = make_color(std::get<0>(color), std::get<1>(color),
-                                     std::get<2>(color));
-        }
-      })));
+      for (size_t j = 0; j < pixel_y; j++) {
+        int value = compute_pixel(c + 4.0 * j / pixel_y - 2.0);
+        std::tuple<size_t, size_t, size_t> color = get_rgb(value);
+        pbm->row(i)[j] = make_color(std::get<0>(color), std::get<1>(color),
+                                    std::get<2>(color));
+      }
+    })));
   }
 
-  auto futures2 = co_await std::move(hpx::when_all(std::move(futures)));
+  auto futures2 = co_await hpx::when_all(futures);
 
-  co_return ;
+  co_return;
 }
 
 int main(int args, char** argv) {
@@ -59,7 +59,7 @@ int main(int args, char** argv) {
   std::chrono::duration<double> diff = end - start;
   std::cout << pixel_x * pixel_y << "," << diff.count() << std::endl;
 
-  pbm.save("image_future.pbm");
+  pbm.save("image_future_coroutine.pbm");
 
   return EXIT_SUCCESS;
 }
